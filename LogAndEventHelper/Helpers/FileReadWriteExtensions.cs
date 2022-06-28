2784fad14376e25df701ac561e70dff7b6fca505
Archery <archerymask@gmail.com>
2022-06-28 15:47:53 +0300

    Added Jaro text check

--- LogAndEventHelper/Helpers/FileReadWriteExtensions.cs
+++ LogAndEventHelper/Helpers/FileReadWriteExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
 
 namespace Mew.Helpers {
@@ -37,7 +38,7 @@ namespace Mew.Helpers {
                 using var sr = new StreamReader(file_info.FullName, true);
                 return sr.ReadToEnd();
             }
-            
+
             encoding ??= PackagingExtensions.DefaultEncoding;
             using (var sr = new StreamReader(file_info.FullName, encoding, detect_encoding)) {
                 return sr.ReadToEnd();
@@ -76,6 +77,78 @@ namespace Mew.Helpers {
 
         public static FileInfo WriteBytesToFile(this FileInfo goal_file, byte[] bytes) => WriteBytesToFile(bytes, goal_file);
 
-        public static FileInfo GetFile(this DirectoryInfo dir, string file_name) => new FileInfo($"{dir.FullName}{file_name}");
+        public static FileInfo GetFile(this DirectoryInfo dir, string file_name) => new FileInfo("{dir.FullName}{file_name}");
+    }
+
+    public static class TextComparer {
+        /// <summary>
+        /// To compare 2 strings or words using "Jaro distance" metric
+        /// distance = 1 − similarity
+        /// </summary>
+        /// <param name="s1"></param>
+        /// <param name="s2"></param>
+        /// <returns>0 - the same, 1 - nothing similar</returns>
+        public static double JaroSimilarity(string s1, string s2) {
+            if (s1 == s2) return 1;
+            if (string.IsNullOrEmpty(s1) || string.IsNullOrEmpty(s1)) return 0;
+
+            // theoretical distance
+            var distance = (int) Math.Floor((Math.Max(s1.Length, s2.Length)-1) / 2.0);
+
+            // get common characters
+            var commons1 = GetCommonCharacters(s1, s2, distance);
+            var commons2 = GetCommonCharacters(s2, s1, distance);
+            //AppLogAndEventHelper.Instance.RaiseDebugInfo(commons1, commons2);
+            if (string.IsNullOrEmpty(commons1) || string.IsNullOrEmpty(commons2)) return 0;
+
+            // calculate transpositions
+            var transpositions = 0.0;
+            for (var i = 0; i < Math.Min(commons1.Length, commons2.Length); i++)
+                if (commons1[i] != commons2[i])
+                    transpositions++;
+            transpositions /= 2.0;
+            //AppLogAndEventHelper.Instance.RaiseDebugInfo(nameof(transpositions),transpositions);
+
+            // return the Jaro distance
+            return (1 + (double)commons1.Length / s1.Length + (double)commons2.Length / s2.Length - transpositions / commons1.Length) / 3.0;
+        }
+
+        private static string GetCommonCharacters(string s1, string s2, int allowed_distance) {
+            var c2 = s2.ToCharArray();
+            var commonCharacters = new StringBuilder();
+
+            for (var i = 0; i < s1.Length; i++) {
+                // compare if char does match inside given allowedDistance
+                // and if it does add it to commonCharacters
+                for (var j = Math.Max(0, i - allowed_distance); j < Math.Min(i + allowed_distance + 1, s2.Length); j++) {
+                    if (c2[j] != s1[i]) continue;
+                    commonCharacters.Append(s1[i]);
+                    c2[j] = ' ';
+                    break;
+                }
+            }
+
+            return commonCharacters.ToString();
+        }
+
+        private static int GetPrefixLength(string s1, string s2, int min_prefix_length = 4) {
+            var n = Math.Min(Math.Min(min_prefix_length, s1.Length), s2.Length);
+
+            for (var i = 0; i < n; i++) {
+                if (s1[i] != s2[i]) // return index of first occurrence of different characters 
+                    return i;
+            }
+
+            // first n characters are the same   
+            return n;
+        }
+
+        public static double JaroWinkler(string s1, string s2, double prefix_scale = 0.1) {
+            var jaro_distance = JaroSimilarity(s1, s2);
+            var prefix_length = GetPrefixLength(s1, s2);
+            AppLogAndEventHelper.Instance.RaiseDebugInfo(nameof(prefix_length),prefix_length);
+
+            return jaro_distance + prefix_length * prefix_scale * (1.0 - jaro_distance);
+        }
     }
 }
---
+++ LogAndEventHelperTests/Helpers/TextComparerTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+
+namespace Mew.Helpers.Tests {
+    [TestClass]
+    public class TextComparerTests {
+        [TestMethod]
+        [DataRow("CRATE", "TRACE", 0.73)]
+        [DataRow("DWAYNE", "DUANE", 0.82)]
+        [DataRow("MARTHA", "MARHTA", 0.94)]
+        [DataRow("DIXON", "DICKSONX", 0.76)]
+        [DataRow("ВО ВРЕМЯ ВОЙНЫ ОСТАЛИСЬ БЕЗ РАБОТЫ?", "Работаем во время военного положения", 0.76)]
+        public void JaroSimilarityTest(string s1, string s2, double res) {
+            
+
+            var r = TextComparer.JaroSimilarity(s1, s2);
+            Debug.WriteLine(r);
+            Assert.IsTrue(Math.Abs(r - res) < 0.01);
+        }
+
+        [TestMethod]
+        [DataRow("CRATE", "TRACE", 0.73)]
+        [DataRow("DWAYNE", "DUANE", 0.84)]
+        [DataRow("MARTHA", "MARHTA", 0.96)]
+        [DataRow("DIXON", "DICKSONX", 0.81)]
+        public void JaroWinklerTest(string s1, string s2, double res) {
+            var r = TextComparer.JaroWinkler(s1, s2);
+            Debug.WriteLine(r);
+            Assert.IsTrue(Math.Abs(r - res) < 0.01);
+        }
+
+        [TestMethod]
+        public void MassiveTest() {
+            var ss = new string[] { "01111", "00101", "11111", "00000" };
+
+            var max_similarity = 0.0;
+            var res1 = -1; var res2 = -1;
+            for (var i = 0; i < ss.Length; i++)
+                for (var j = i + 1; j < ss.Length; j++) {
+                    var similarity = TextComparer.JaroWinkler(ss[i], ss[j]);
+                    if (max_similarity > similarity) continue;
+                    max_similarity = similarity;
+                    res1 = i;
+                    res2 = j;
+                }
+            Debug.WriteLine($"Most simular are {res1 + 1} and {res2 + 1}");
+        }
+    }
+}
